cb058a45b12ac8feab45ef4aa820a330ac6a58eb
kiryaremchuk <heilikpd@gmail.com>
2020-09-23 17:53:30 +0300

    url

--- src/environment.js
+++ src/environment.js
@@ -1,6 +1,6 @@
 
 const environment = {
-   backend_url: process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://rollindeuce-backend.herokuapp.com',
+   backend_url: process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://fatality-server.herokuapp.com',
    STEAM_WEB_API_KEY: 'FD36738ABF7E2B4378188F0DAFACD07D'
 }
 
